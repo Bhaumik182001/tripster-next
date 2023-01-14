d96917351577366b9de57a0b8482e3af65b077bc
Bhaumik182001 <bhaumik182001@gmail.com>
2023-01-14 01:59:37 -0500

    added github and discord

--- components/AccessDenied.js
+++ components/AccessDenied.js
@@ -6,7 +6,7 @@ export default function AccessDenied () {
     <div className='flex flex-col w-full  h-90'>
       <div className=' relative h-[600px] w-[600px] align-center justify-center mx-auto'>
       <Image src="https://i.imgur.com/VOHB74Z.png" fill className='object-cover'/>
-        <p className='absolute bottom-20 right-40 mx-auto align-center'>
+        <p className='absolute bottom-20 right-40 text-lg mx-auto align-center'>
           You must be
           <span
           href="/api/auth/signin"
@@ -14,7 +14,7 @@ export default function AccessDenied () {
           e.preventDefault()
           signIn()
        }}
-          className='cursor-pointer hover:scale-105 text-black bg-white text-lg px-2 py-1 rounded-lg space-x-1 mx-3'>
+          className='cursor-pointer hover:scale-105 text-black bg-white text-xl font-semibold px-2 py-1 rounded-lg space-x-1 mx-3'>
             Signed In
           </span>
            to view this page
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth"
 
 import GoogleProvider from "next-auth/providers/google"
-
+import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
     
@@ -11,6 +12,14 @@ export default NextAuth({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
       }),
+      DiscordProvider({
+        clientId: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET
+      }),
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      })
       // Sign in with passwordless email link
       
     ],
