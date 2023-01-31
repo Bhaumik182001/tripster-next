bc3e029df224ddebdce0885d74b73e37a09deb77
Bhaumik182001 <bhaumik182001@gmail.com>
2023-01-30 22:28:12 -0500

    large button made functional

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,7 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
+import { useRouter } from 'next/router'
+
 
 function LargeCard({title, description, buttonText, img}) {
+
+  const router = useRouter();
+
+
+  const pushSearchGreatList =() => {
+    router.push({
+      pathname: '/search',
+      query: {
+        input: "Paradise",
+        startingDate: new Date().toDateString(),
+        lastDate: new Date().toDateString(),
+        guests: 1
+      }
+    })
+  }
+
   return (
     <div className='relative mt-4'>
         <div className='relative h-96 min-w-[300px] py-16'>
@@ -10,7 +28,7 @@ function LargeCard({title, description, buttonText, img}) {
         <div className='absolute top-32 left-12 space-y-2'>
         <h2 className='text-black text-4xl flex-wrap max-w-[200px]'>{title}</h2>
         <h3 className='text-black'>{description}</h3>
-        <button className='bg-black hover:scale-105 active:scale-95 transform transition duration-200 text-gray-300 hover:text-white  py-2 px-4 rounded-md text-sm'>{buttonText}</button>
+        <button onClick={()=>pushSearchGreatList()} className='bg-black hover:scale-105 active:scale-95 transform transition duration-200 text-gray-300 hover:text-white  py-2 px-4 rounded-md text-sm'>{buttonText}</button>
         </div>
         
     </div>
